307260049a96202caf94b86c303b0c943432f262
Clayton McCray <cpmdmccray@gmail.com>
2021-03-15 23:13:20 -0500

    * Switch to always initialize values as pointers * Register plain functions to metaData.singleTest

--- generate/generate.go
+++ generate/generate.go
@@ -4,47 +4,123 @@ import (
 	"bytes"
 	_ "embed"
 	"errors"
+	"fmt"
 	"go/ast"
 	"go/format"
 	"io"
+	"log"
 	"text/template"
 
 	"github.com/ClaytonMcCray/fuzzy/elements"
 )
 
+// TODO: Everything is created as a pointer. If the ast.Expr is not a star expression, we should
+//	dereference the value before using it instead of trying to make it an automatic local value.
+//	This means the template needs to be updated, and the tests.
+
+// TODO:
+// 1) There is no special handling of interface arguments right now.
+// 	* They will probably need to be handled with a directive like "//fuzzy:concrete type Concrete Interface"
+// 		which will do type aliasing to the concrete type.
+// 2) It should probably use goimports instead of go/format.
+
 //go:embed tmpl.go.txt
 var headerTmpl string
 
-type argPair struct {
+type argDescriptor struct {
 	Name, Type string
+	IsPointer  bool
 }
 
 type singleTest struct {
-	FunctionUnderTest            string
-	CompositePtrsToInit          []argPair
-	CompositeAutosToInit         []argPair
-	SimplePtrsToInit             []argPair
-	SimpleAutosToInit            []argPair
-	FunctionArgumentNamesInOrder []string
-	ReceiverName                 argPair
-	ReceiverIsPointer            bool
-	ReceiverIsComposite          bool
+	FunctionUnderTest   string
+	PtrsToInit          []argDescriptor
+	ReceiverName        argDescriptor
+	ReceiverIsPointer   bool
+	ReceiverIsComposite bool
 }
 
 type metaData struct {
 	PackageName         string
 	CompletePackagePath string
-	TestsToGenerate     []singleTest
+	TestsToGenerate     []*singleTest
 }
 
-func Gen(stdout, stderr io.Writer, input *ast.Package) error {
-	fs := elements.NewFuzzySet()
-	ast.Walk(fs, input)
+func (st *singleTest) convertFields(fields []*ast.Field) {
+	for _, arg := range fields {
+		log.Printf("Working on %s", arg.Names[0].Name)
+		if len(arg.Names) < 1 {
+			log.Fatalf("failing in CreateMetaData, arg.Names: %v, too short", arg.Names)
+		}
 
-	return errors.New("not implemented")
+		switch arg.Type.(type) {
+		case *ast.StarExpr:
+			switch arg.Type.(*ast.StarExpr).X.(type) {
+			case *ast.Ident:
+				log.Printf("%s is *ast.StarExpr", arg.Names[0].Name)
+				st.PtrsToInit = append(st.PtrsToInit, argDescriptor{
+					Name:      arg.Names[0].Name,
+					Type:      arg.Type.(*ast.StarExpr).X.(*ast.Ident).Name,
+					IsPointer: true,
+				})
+
+			case *ast.SelectorExpr:
+				log.Printf("%s is *ast.StarExpr (selector)", arg.Names[0].Name)
+				st.PtrsToInit = append(st.PtrsToInit, argDescriptor{
+					Name: arg.Names[0].Name,
+					Type: fmt.Sprintf("%s.%s",
+						arg.Type.(*ast.StarExpr).X.(*ast.SelectorExpr).X.(*ast.Ident).Name,
+						arg.Type.(*ast.StarExpr).X.(*ast.SelectorExpr).Sel.Name),
+
+					IsPointer: true,
+				})
+			}
+
+		case *ast.SelectorExpr:
+			log.Printf("%s is *ast.SelectorExpr", arg.Names[0].Name)
+			log.Printf("%s is not *ast.StarExpr", arg.Names[0].Name)
+			st.PtrsToInit = append(st.PtrsToInit, argDescriptor{
+				Name: arg.Names[0].Name,
+				Type: fmt.Sprintf("%s.%s", arg.Type.(*ast.SelectorExpr).X.(*ast.Ident).Name,
+					arg.Type.(*ast.SelectorExpr).Sel.Name),
+				IsPointer: false,
+			})
+
+		default:
+			log.Printf("%s hit default", arg.Names[0].Name)
+			st.PtrsToInit = append(st.PtrsToInit, argDescriptor{
+				Name: arg.Names[0].Name,
+				Type: arg.Type.(*ast.Ident).Name,
+			})
+		}
+	}
 }
 
-func (md metaData) Do() ([]byte, error) {
+// CreateMetaData takes in a FuzzySet and converts its data into the structure metaData,
+// needed to execute the test function templates.
+func CreateMetaData(fs *elements.FuzzySet) *metaData {
+	md := &metaData{
+		PackageName:         fs.PackageName,
+		CompletePackagePath: fs.CompletePackagePath,
+	}
+
+	// Create a singleTest for each plain function under test.
+	fs.Inspect(elements.PlainFuncs, func(fd *ast.FuncDecl) {
+		curTest := &singleTest{
+			FunctionUnderTest: fd.Name.Name,
+		}
+
+		curTest.convertFields(fd.Type.Params.List)
+
+		md.TestsToGenerate = append(md.TestsToGenerate, curTest)
+	})
+
+	// TODO: handle the receiver functions
+
+	return md
+}
+
+func (md *metaData) Do() ([]byte, error) {
 	b := &bytes.Buffer{}
 	tmpl, err := template.New("package_fuzzy_test").Parse(headerTmpl)
 	if err != nil {
@@ -57,4 +133,8 @@ func (md metaData) Do() ([]byte, error) {
 	}
 
 	return format.Source(b.Bytes())
+}
+
+func Gen(stdout, stderr io.Writer, input *ast.Package) error {
+	return errors.New("not implemented")
 }
--- generate/generate_test.go
+++ generate/generate_test.go
@@ -1,43 +1,41 @@
 package generate
 
 import (
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"testing"
+
+	"github.com/ClaytonMcCray/fuzzy/elements"
 )
 
+// TestTemplate doesn't make any strong assertions about the output. It needs to
+// be run in verbose mode (go test -v ./generate) so that the output can be inspected.
 func TestTemplate(t *testing.T) {
-	functionsToTest := []singleTest{
+	functionsToTest := []*singleTest{
 		{
 			FunctionUnderTest: "MyFunction",
-			CompositePtrsToInit: []argPair{
+			PtrsToInit: []argDescriptor{
 				{Type: "myType", Name: "one"},
-			},
-
-			SimpleAutosToInit: []argPair{
+				{Type: "int", Name: "one"},
+				{Type: "float64", Name: "two"},
 				{Type: "int", Name: "two"},
-				{Type: "float64", Name: "three"},
+				{Type: "float64", Name: "three", IsPointer: true},
 			},
-
-			FunctionArgumentNamesInOrder: []string{"one", "two", "three"},
-			ReceiverName:                 argPair{Type: "myRecvrType", Name: "recvr"},
-			ReceiverIsPointer:            true,
-			ReceiverIsComposite:          true,
+			ReceiverName:        argDescriptor{Type: "myRecvrType", Name: "recvr"},
+			ReceiverIsPointer:   true,
+			ReceiverIsComposite: true,
 		},
 		{
 			FunctionUnderTest: "PlainFunction",
-			SimplePtrsToInit: []argPair{
-				{Type: "int", Name: "one"},
-				{Type: "float64", Name: "two"},
-			},
-			CompositeAutosToInit: []argPair{
+			PtrsToInit: []argDescriptor{
 				{Type: "CompAuto", Name: "three"},
-				{Type: "bytes.Buffer", Name: "four"},
+				{Type: "bytes.Buffer", Name: "four", IsPointer: true},
 			},
-
-			FunctionArgumentNamesInOrder: []string{"one", "two", "four", "three"},
 		},
 	}
 
-	in := metaData{
+	in := &metaData{
 		PackageName:         "mypackage",
 		CompletePackagePath: "gitter.com/me/mypackage",
 		TestsToGenerate:     functionsToTest,
@@ -50,4 +48,88 @@ func TestTemplate(t *testing.T) {
 	}
 
 	t.Logf("%s", b)
+}
+
+func setupAndParse(t *testing.T, toParse string) *ast.File {
+	expr, err := parser.ParseFile(token.NewFileSet(), "src.go", toParse, 0)
+
+	if err != nil {
+		t.Fatalf("got %s trying to parse toParse; expected nil", err)
+	}
+
+	return expr
+}
+
+func assertEquivSingleTest(t *testing.T, actualTest, est *singleTest) {
+	if actualTest.FunctionUnderTest != est.FunctionUnderTest {
+		t.Errorf("expected %s, got %s", est.FunctionUnderTest, actualTest.FunctionUnderTest)
+	}
+
+	for i, actual := range actualTest.PtrsToInit {
+		if actual != est.PtrsToInit[i] {
+			t.Errorf("expected %v, got %v", est.PtrsToInit[i], actual)
+		}
+	}
+
+	if actualTest.ReceiverName != est.ReceiverName {
+		t.Errorf("expected %v, got %v", est.ReceiverName, actualTest.ReceiverName)
+	}
+
+	if actualTest.ReceiverIsPointer {
+		t.Errorf("got true for ReceiverIsPointer, should be false")
+	}
+
+	if actualTest.ReceiverIsComposite {
+		t.Errorf("got true for ReceiverIsComposite, should be false")
+	}
+}
+
+func TestCreateMetaDataPlainFunction(t *testing.T) {
+	src := `
+package main
+
+func TestMe(x int, y *float64, z bytes.Buffer, a *ast.Ident) error {
+	return nil
+}
+
+func TestMe2(b, c int) {}
+`
+	parsed := setupAndParse(t, src)
+	fs := elements.NewFuzzySet("main", "my/main")
+	ast.Walk(fs, parsed)
+
+	md := CreateMetaData(fs)
+
+	if md.CompletePackagePath != "my/main" {
+		t.Errorf("wrong CompletePackagePath, got %s", md.CompletePackagePath)
+	}
+
+	if md.PackageName != "main" {
+		t.Errorf("wrong PackageName, got %s", md.PackageName)
+	}
+
+	actualTest := md.TestsToGenerate[0]
+
+	est := &singleTest{
+		FunctionUnderTest: "TestMe",
+		PtrsToInit: []argDescriptor{
+			{"x", "int", false},
+			{"y", "float64", true},
+			{"z", "bytes.Buffer", false},
+			{"a", "ast.Ident", true},
+		},
+	}
+
+	assertEquivSingleTest(t, actualTest, est)
+
+	actualTest = md.TestsToGenerate[1]
+	est = &singleTest{
+		FunctionUnderTest: "TestMe2",
+		PtrsToInit: []argDescriptor{
+			{"b", "int", false},
+			{"c", "int", false},
+		},
+	}
+
+	assertEquivSingleTest(t, actualTest, est)
 }
