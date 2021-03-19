37111e7b170b30d99ac523fd69e9ad5d1f7f8523
Clayton McCray <cpmdmccray@gmail.com>
2021-03-18 20:50:03 -0500

    add entry hook for binary

--- main.go
+++ main.go
@@ -1,25 +1,59 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"flag"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"io"
+	"io/fs"
 	"log"
+	"os"
+	"strings"
+
+	"github.com/ClaytonMcCray/fuzzy/generate"
 )
 
 func main() {
+	dirPath := flag.String("i", "", "The path to the directory you wish to fuzz. Required.")
+	packageToFuzz := flag.String("package", "", "The package to fuzz in -i. Required.")
+	outputFile := flag.String("o", "fuzzy_fuzz_test.go", "The path to the output file. It should end in _test.go")
+	flag.Parse()
+
+	if *dirPath == "" || *packageToFuzz == "" {
+		log.Fatal("See usage. -i must be set. -package must be set.")
+	}
+
+	f, err := os.Create(*outputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output := bufio.NewWriter(f)
+	fset := token.NewFileSet()
+	parsed, err := parser.ParseDir(fset, *dirPath, func(fi fs.FileInfo) bool {
+		return !strings.HasSuffix(fi.Name(), "_test.go")
+	}, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toFuzz, ok := parsed[*packageToFuzz]
+	if !ok {
+		log.Fatalf("Failed to parse package %s", *packageToFuzz)
+	}
+
+	if err = Run(toFuzz, output, os.Stderr); err != nil {
+		log.Fatal(err)
+	}
 }
 
-// TODO: when parsing, make sure to filter out *Test.go files, and note in help dialogue
-//	 that fuzz tests should be written to a *Test.go file.
-func Run(stdin io.Reader, stdout, stderr io.Writer, args []string) error {
-	log.SetOutput(stdout)
-	flags := flag.NewFlagSet(args[0], flag.PanicOnError)
-	flags.Parse(args[1:])
-
-	if flags.NArg() < 2 {
-		log.Printf("%d", flags.NArg())
-		return errors.New("Usage: fuzzy DIRECTORY PACKAGE [-o OUTPUT]")
+func Run(input *ast.Package, stdout, stderr io.Writer) error {
+	err := generate.Gen(stdout, stderr, input)
+	if err != nil {
+		return err
 	}
+
 	return nil
 }
